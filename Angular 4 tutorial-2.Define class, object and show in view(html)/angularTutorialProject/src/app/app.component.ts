3519af6861a84a1a59543f48984609b81b8fe8a4
Nguyen Duc Hoang <sunlight4d@gmail.com>
2017-04-10 21:51:34 +0700

    update

---
+++ Angular 4 tutorial-2.Define class, object and show in view(html)/angularTutorialProject/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { User } from './models/user'
+
+@Component({
+  selector: 'my-app',
+  template: `<h1>Hello {{title}}</h1>
+            <h2>Hello {{user.name}}. This is your details: </h2>
+            <div><label>id: </label>{{user.id}}</div>
+            <div><label>name: </label>{{user.name}}</div>
+            <div><label>email: </label>{{user.email}}</div>
+`,
+})
+export class AppComponent  {
+  title = 'Angular Tutorial';
+  user:User = {
+      id: 1,
+      name: 'Nguyen Duc Hoang',
+      age: 30,
+      email: "sunlight4d@gmail.com"
+  }
+}
